86a5d94346ef1764644e77874a62609e91714db3
carlsondultra <carlsondultra@users.noreply.github.com>
2023-10-05 14:13:54 -0400

    adding react hook form with yup schema validation

--- apps/web/pages/form/page.tsx
+++ apps/web/pages/form/page.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
-export default function page() {
+const SignupSchema = yup.object().shape({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  age: yup.number().required().positive().integer(),
+  website: yup.string().url()
+});
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(SignupSchema)
+  });
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+
   return (
-    <div>page</div>
-  )
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>First Name</label>
+        <input {...register("firstName")} />
+        {errors.firstName && <p>{errors.firstName.message}</p>}
+      </div>
+      <div style={{ marginBottom: 10 }}>
+        <label>Last Name</label>
+        <input {...register("lastName")} />
+        {errors.lastName && <p>{errors.lastName.message}</p>}
+      </div>
+      <div>
+        <label>Age</label>
+        <input type="number" {...register("age", { valueAsNumber: true })} />
+        {errors.age && <p>{errors.age.message}</p>}
+      </div>
+      <div>
+        <label>Website (example: https://www.google.ca)</label>
+        <input {...register("website")} />
+        {errors.website && <p>{errors.website.message}</p>}
+      </div>
+      <input type="submit" />
+    </form>
+  );
 }
