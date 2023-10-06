e9b2f4c783446b8ae7dd81cb16db51bfa35b757b
carlsondultra <carlsondultra@users.noreply.github.com>
2023-10-05 21:42:23 -0400

    adding update user and delete user functionality

--- apps/web/pages/form/userCard.tsx
+++ apps/web/pages/form/userCard.tsx
@@ -1,6 +1,11 @@
-import {Card, Button, Form} from 'react-bootstrap';
+// import {Button, Card, Form} from 'react-bootstrap';
 import { useState } from 'react';
 import { supabase } from './supabaseClient';
+
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+
 
 function UserCard(props) {
     const user = props.user;
@@ -11,29 +16,64 @@ function UserCard(props) {
     const [ age, setAge ] = useState(user.age);
     const [ website, setWebsite ] = useState(user.website);
 
+    async function updateUser() {
+        try {
+            const { data, error } = await supabase
+            // updating users table
+            .from("users")
+            .update({
+                fname: fname,
+                lname: lname,
+                age: age,
+                website: website
+            })
+            .eq("id", user.id) //looking for the row that equals the ID currently being edited
+            if (error) throw error;
+            window.location.reload() //reload page once user has been created
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
+    async function deleteUser() {
+        try {
+            const { data, error } = await supabase
+            // deleting from users table
+            .from("users")
+            .delete()
+            .eq("id", user.id) //looking for the row that equals the ID currently being deleted
+            if (error) throw error;
+            window.location.reload() //reload page once user has been created
+        } catch (error) {
+            alert(error.message);
+        }
+    }
+
     return (
         <Card style={{width: "18rem"}}>
             <Card.Body>
                 { editing == false ?
                     <>
                         <Card.Title>{user.fname}</Card.Title>
-                        <Card.Text>{user.description}</Card.Text>
-                        {/* <Button variant="danger">Delete user</Button>
-                        <Button variant="secondary" onClick={() => setEditing(true)}>Edit user</Button> */}
+                        <Card.Text>{user.lname}</Card.Text>
+                        <Card.Text>{user.age}</Card.Text>
+                        <Card.Text>{user.website}</Card.Text>
+                        <Button variant="danger" onClick={() => deleteUser()}>Delete user</Button>
+                        <Button variant="secondary" onClick={() => setEditing(true)}>Edit user</Button>
                     </>
                 :
                     <>
                         <h4>Editing user</h4>
                         <Button size="sm" onClick={() => setEditing(false)}>Go Back</Button>
                         <br></br>
-                        <Form.Label>user fname</Form.Label>
+                        <Form.Label>First Name</Form.Label>
                         <Form.Control
                             type="text"
                             id="fname"
                             defaultValue={user.fname}
                             onChange={(e) => setFName(e.target.value)}
                         />
-                        <Form.Label>user Description</Form.Label>
+                        <Form.Label>Last Name</Form.Label>
                         <Form.Control
                             type="text"
                             id="lname"
@@ -41,7 +81,21 @@ function UserCard(props) {
                             onChange={(e) => setLName(e.target.value)}
                         />
                         <br></br>
-                        {/* <Button onClick={() => updateuser()}>Update user in Supabase DB</Button> */}
+                        <Form.Control
+                            type="text"
+                            id="age"
+                            defaultValue={user.age}
+                            onChange={(e) => setAge(e.target.value)}
+                        />
+                        <br></br>
+                        <Form.Control
+                            type="text"
+                            id="website"
+                            defaultValue={user.website}
+                            onChange={(e) => setWebsite(e.target.value)}
+                        />
+                        <br></br>
+                        <Button onClick={() => updateUser()}>Update User</Button>
                     </>
                 }
             </Card.Body>
